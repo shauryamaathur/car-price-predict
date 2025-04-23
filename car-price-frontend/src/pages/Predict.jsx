69438186d4dd8d94902f2ffaf8fc439f8fcd8cdb
Shaurya Maathur <shauryamaathur@gmail.com>
2025-04-24 01:23:31 +0530

    Updated backend API URL to live server

--- car-price-frontend/src/pages/Predict.jsx
+++ car-price-frontend/src/pages/Predict.jsx
@@ -38,7 +38,7 @@ const Predict = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://car-price-predict-g03p.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
